19b00eae988206a4c086c39387aab87f2602d90c
sebastianaugustolopes <sebastianaugustolopes@gmail.com>
2025-07-19 21:10:41 -0300

    Add meals table with enums and align user fields for consistency

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,20 +1,37 @@
-import { date, integer, pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
+import { date, integer, json, pgEnum, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users', {
   id: uuid().primaryKey().defaultRandom(),
-  name: varchar({length: 255}).notNull(),
-  email: varchar('email', { length: 255 }).notNull().unique(),
-  password: varchar('password', { length: 255 }).notNull(),
-  goal: varchar('goal', { length: 8 }).notNull(),
-  gender: varchar('gender', { length: 6 }).notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  goal: varchar({ length: 8 }).notNull(),
+  gender: varchar({ length: 6 }).notNull(),
   birthDate: date('birth_date').notNull(),
-  height: integer('height').notNull(),
-  weight: integer('weight').notNull(),
+  height: integer().notNull(),
+  weight: integer().notNull(),
   activityLevel: integer('activity_level').notNull(),
-
   // Goals
-  calories: integer('calories').notNull(),
-  proteins: integer('proteins').notNull(),
-  carbohydrates: integer('carbohydrates').notNull(),
-  fats: integer('fats').notNull(),
+  calories: integer().notNull(),
+  proteins: integer().notNull(),
+  carbohydrates: integer().notNull(),
+  fats: integer().notNull(),
+});
+
+export const mealStatus = pgEnum('meal_status', ['uploading','processing', 'success', 'failed']);
+
+export const mealInputType = pgEnum('meal_input_type', ['audio', 'picture']);
+
+export const mealsTable = pgTable('meals', {
+  id: uuid().primaryKey().defaultRandom(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  status: mealStatus().notNull(),
+  inputType: mealInputType('input_type').notNull(),
+  inputFileKey: varchar('input_file_key', { length: 255 }).notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  icon: varchar({ length: 100 }).notNull(),
+  foods: json(),  
+  createdAt: timestamp('created_at').notNull().defaultNow(),
 });
